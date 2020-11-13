f0a28a707c88dcc74cd46141609db0246a25c97a
Taehee Choi <obbcth@naver.com>
2020-11-13 17:45:36 +0900

    Add function : automatic ear detection
    Cause response value is not very clear, I need to set timeout and save previous state.

--- AirPodsClient.py
+++ AirPodsClient.py
@@ -2,12 +2,44 @@ from bleak import discover
 from time import time,sleep
 from infi.systray import SysTrayIcon
 import asyncio, threading, bluetooth, webbrowser
+import win32api
 
 def open_homepage(systray):
     webbrowser.open('https://github.com/obbcth/AirPodsClient', new=2)
 
-menu_options = (("Visit GitHub", None, open_homepage),)
-systray = SysTrayIcon("white.ico", "Scanning devices...", menu_options)
+def switch_tray(systray):
+    pass
+
+AED = False # Automatic Ear Detection
+AED_Flag = True
+
+def automatic_ear_detection(systray):
+    global AED 
+    if AED:
+        menu_options = (
+            ("Visit GitHub", None, open_homepage),
+            ("Automatic Ear Detection : Off", None, automatic_ear_detection),
+            ("Switch Tray Icon", None, switch_tray),
+        )
+        AED = False
+    else:
+        menu_options = (
+            ("Visit GitHub", None, open_homepage),
+            ("Automatic Ear Detection : On", None, automatic_ear_detection),
+            ("Switch Tray Icon", None, switch_tray),
+        )
+        AED = True
+    systray.update(menu_options = menu_options)
+
+
+menu_options = (
+    ("Visit GitHub", None, open_homepage),
+    ("Automatic Ear Detection : Off", None, automatic_ear_detection),
+    ("Switch Tray Icon", None, switch_tray),
+)
+
+
+systray = SysTrayIcon("AirPods.ico", "Scanning devices...", menu_options)
 systray.start()
 
 async def run():
@@ -28,6 +60,10 @@ async def run():
             result['addr'] = d.address
             
             hexData = d.metadata['manufacturer_data'][76].hex()
+            result['status'] = 1
+
+            # 12th and 13th letter shows:
+            # the charge of the left and right pods. Under unknown circumstances, they are right and left instead (see isFlipped). Values between 0 and 10 are battery 0-100%; Value 15 means it's disconnected
             flipped = isFlipped(hexData)
             if flipped:
                 result['left'] = "" + hexData[12]
@@ -35,12 +71,33 @@ async def run():
             else:
                 result['left'] = "" + hexData[13]
                 result['right'] = "" + hexData[12]
+
+            # 15th letter shows:
+            # the charge of the case. Values between 0 and 10 are battery 0-100%; Value 15 means it's disconnected
             result['case'] = "" + hexData[15]
+
+            # 14th letter shows:
+            # the "in charge" status. Bit 0 (LSB) is the left pod; Bit 1 is the right pod; Bit 2 is the case. Bit 3 might be case open/closed but I'm not sure and it's not used
             result['charging_case'] = "" + hexData[14]
             result['charging_left'] = "" + hexData[14]
             result['charging_right'] = "" + hexData[14]
+
+            # 7th letter shows:
+            # the AirPods model (E=AirPods pro)
             result['model'] = "" + hexData[7]
-            result['status'] = 1
+
+            # 11th letter shows:
+            # 1 when both AirPods are removed from ears
+            # b when both AirPods are in ears
+            # 3 (Left Plugged) or 9 (Right Plugged) when only one AirPod is removed
+            # 2 when one AirPod is in the case charging and other one is in ear
+            # 0 when one AirPod is in the case charging and other one is not in ear
+            result['wearing'] = "" + hexData[11]
+
+            # 21th letter shows: 
+            # 5 when playing
+            # 4 when paused
+            result['playing'] = "" + hexData[21]
 
             result = parseValues(result, flipped)
 
@@ -81,6 +138,14 @@ def parseValues(result, flipped):
         result['charging_right'] = (chargeStatus & 0b00000010) != 0
     result['charging_case'] = (chargeStatus & 0b00000100) != 0 # Only right value returns when AirPods are in case.
 
+    result['wearing'] = int(result['wearing'], 16)
+    if result['wearing'] != 11:
+        result['wearing'] = 0
+    else:
+        result['wearing'] = 1
+    
+    result['playing'] = int(result['playing'], 16) - 4
+
     return result
 
 def EmptyResult():
@@ -96,6 +161,8 @@ def EmptyResult():
     result["rssi"] = -670
     result["addr"] = ""
     result['model'] = ""
+    result['wearing'] = 0
+    result['playing'] = 0
     return result
 
 def fetch_status():
@@ -114,8 +181,19 @@ while True:
     status = status.replace('False', '')
 
     if result['model'] == "Pro":
-        systray.update("lightblue.ico", status)
+        systray.update("AirPodsPro.ico", status)
 
     if result['model'] == "Unknown":
-        systray.update("lightgreen.ico", status)
+        systray.update("AirPods.ico", status)
+    
+    if AED:
+        if result['wearing'] == 0:
+            if AED_Flag:
+                win32api.keybd_event(0xB3, 34)
+                AED_Flag = False
+
+        if result['wearing'] == 1:
+            if AED_Flag == False:
+                win32api.keybd_event(0xB3, 34)
+                AED_Flag = True
     
