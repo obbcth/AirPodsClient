4877d5dd4a6ec0ab539cfb0d3917411bf5ecc8cb
Taehee Choi <obbcth@naver.com>
2020-11-21 13:47:56 +0900

    Clean unused files

--- clean.bat
+++
@@ -1,4 +0,0 @@
-rmdir /s /q __pycache__
-rmdir /s /q build
-rmdir /s /q dist
-del /s /q AirPodsClient.spec
--- make.bat
+++
@@ -1,3 +0,0 @@
-pyinstaller AirPodsClient.py -i ./icons/AirPods.ico --hidden-import=pkg_resources.py2_warn --clean --noconfirm --noconsole --onefile --add-data ./icons;icons
-
-pause
